7c33adf6ff26f8e83bf4e40a632a23344608c456
Rahul255 <rahulmohan255@gmail.com>
2021-06-25 20:25:47 +0530

    file added

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,33 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+// Step 1: Import the useStaticQuery hook and graphql tag
+import { Link, useStaticQuery,graphql } from 'gatsby'
 import { 
     container,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText
+  navLinkText,
+  siteTitle,
  } from './layout.module.css'
 
 const Layout = ({pageTitle, children}) => {
+    /* Step 2: Use the useStaticQuery hook and
+    graphql tag to query for data
+    (The query gets run at build time) */
+    const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
     return (
         <main className={container}>
-            <title>{pageTitle}</title>
+            {/* Step 3: Use the data in your component */}
+            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+            <p className={siteTitle}>{data.site.siteMetadata.title}</p>
             <nav>
                 <ul className={navLinks}>
                     <li className={navLinkItem}>
@@ -19,6 +35,9 @@ const Layout = ({pageTitle, children}) => {
                     </li>
                     <li className={navLinkItem}>
                         <Link to="/about" className={navLinkText}>About</Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/blog" className={navLinkText}>Blog</Link>
                     </li>
                 </ul>
             </nav>
