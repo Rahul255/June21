8b4d7969f888df016bf3419761a2ca709d3c9ce0
Rahul255 <rahulmohan255@gmail.com>
2021-06-26 08:51:10 +0530

    file added

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,34 @@
 import * as React from 'react'
+// Step 1: Import the graphql tag
+import {graphql} from 'gatsby'
 import Layout from '../components/layout'
-import {siteTitle} from '../components/layout.module.css'
 
 
-const BlogPage = () => {
+const BlogPage = ({data}) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here</p>
+      { /* Step 3: Use the data in your component*/ }
+      <ul>
+        {
+          data.allFile.nodes.map(node => (
+            <li key={node.name}>
+              {node.name}
+            </li>
+          ))
+        }
+      </ul>
     </Layout>
   )
 }
+// Step 2: Export a page query
+export const query = graphql `
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export default BlogPage
